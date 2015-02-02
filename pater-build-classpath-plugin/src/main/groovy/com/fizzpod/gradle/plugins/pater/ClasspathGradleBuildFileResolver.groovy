1f503131cf94e4aa3abda92c7d916f09ea9277c4
Andrew Dunn <andy.dunn@expensys.com>
2015-02-02 13:43:32 +0000

    Fleshing out classpath implementation

--- pater-build-classpath-plugin/src/main/groovy/com/fizzpod/gradle/plugins/pater/ClasspathGradleBuildFileResolver.groovy
+++ pater-build-classpath-plugin/src/main/groovy/com/fizzpod/gradle/plugins/pater/ClasspathGradleBuildFileResolver.groovy
@@ -5,24 +5,52 @@ import java.util.regex.Pattern
 import org.gradle.api.Project
 import org.reflections.Reflections
 import org.reflections.scanners.ResourcesScanner
-import org.reflections.util.ClasspathHelper
-import org.reflections.util.ConfigurationBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 public class ClasspathGradleBuildFileResolver implements GradleBuildFileResolver {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClasspathGradleBuildFileResolver.class);
 
 	Collection<URI> findBuildFiles(Project project) {
 		Set<String> buildFiles = scanForBuildFiles();
-		
-		return Arrays.asList(project.uri(new File("C:/Users/andyd_000/Documents/workspace-ggts-3.6.3.SR1/gradle-parent/src/test/resources/build.gradle")));
+		LOGGER.info("Discovered build files {}", buildFiles);
+		Collection<File> exportedBuildFiles = exportBuildFiles(buildFiles);
+		return transformBuildFilesToUri(project, exportedBuildFiles);
 	}
 	
 	private Set<String> scanForBuildFiles(Project project) {
 		
-		Reflections reflections = new Reflections("META-INF.gradle-parent", new ResourcesScanner());
+		Reflections reflections = new Reflections("META-INF.pater-build", new ResourcesScanner());
 		
 		Set<String> buildFiles =
 		reflections.getResources(Pattern.compile(".*\\.gradle"));
 		return buildFiles;
+	}
+	
+	private Collection<File> exportBuildFiles(Collection<String> classpathBuildFiles) {
+		List<File> buildFiles = new LinkedList<>();
+		for(String classpathBuildFile: classpathBuildFiles) {
+			File buildFile = exportBuildFile(classpathBuildFile);
+			if(buildFile != null && buildFile.exists()) {
+				buildFiles.add(buildFile);
+			} else {
+				LOGGER.warn("Could not export build file {}", classpathBuildFile);
+			}
+		}
+		return buildFiles;
+	}
+	
+	private File exportBuildFile(classpathBuildFile) {
+		return new File("C:/Users/andyd_000/Documents/workspace-ggts-3.6.3.SR1/gradle-parent/src/test/resources/build.gradle");
+	}
+	
+	private Collection<URI> transformBuildFilesToUri(Project project, Collection<File> buildFiles) {
+		List<URI> uris = new ArrayList<>();
+		for(File buildFile: buildFiles) {
+			uris.add(project.uri(buildFile));
+		}
+		return uris;
 	}
 	
 }
