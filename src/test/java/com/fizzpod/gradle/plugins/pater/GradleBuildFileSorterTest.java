7f90a0b479f9de130b5723f929179ea3808b4a4b
boxheed <boxheed@users.noreply.github.com>
2015-02-04 09:42:02 +0000

    Fleshed out unit tests.

--- src/test/java/com/fizzpod/gradle/plugins/pater/GradleBuildFileSorterTest.java
+++ src/test/java/com/fizzpod/gradle/plugins/pater/GradleBuildFileSorterTest.java
@@ -44,6 +44,36 @@ public class GradleBuildFileSorterTest {
 		assertUri("abc-xyz.gradle", sortedUris.get(1));
 	}
 	
+	
+	@Test
+	public void testSameFirstLevelSecondLevelSameButLongerNameSorting() {
+		Collection<URI> uris = createFileUris("abc-xyz.gradle", "abc-xyzdef.gradle");
+		FileNameGradleBuildFileSorter sorter = new FileNameGradleBuildFileSorter();
+		List<URI> sortedUris = new LinkedList<>(sorter.sortBuildFiles(null, uris));
+		assertUri("abc-xyz.gradle", sortedUris.get(0));
+		assertUri("abc-xyzdef.gradle", sortedUris.get(1));
+	}
+	
+	@Test
+	public void testMulitpleMixedNameSorting() {
+		Collection<URI> uris = createFileUris("abc.gradle", "def-xyz.gradle", "abc-xyz.gradle", "def.gradle");
+		FileNameGradleBuildFileSorter sorter = new FileNameGradleBuildFileSorter();
+		List<URI> sortedUris = new LinkedList<>(sorter.sortBuildFiles(null, uris));
+		assertUri("abc.gradle", sortedUris.get(0));
+		assertUri("def.gradle", sortedUris.get(1));
+		assertUri("abc-xyz.gradle", sortedUris.get(2));
+		assertUri("def-xyz.gradle", sortedUris.get(3));
+	}
+	
+	@Test
+	public void testCaseInsensitiveNameSorting() {
+		Collection<URI> uris = createFileUris("a.gradle", "B.gradle");
+		FileNameGradleBuildFileSorter sorter = new FileNameGradleBuildFileSorter();
+		List<URI> sortedUris = new LinkedList<>(sorter.sortBuildFiles(null, uris));
+		assertUri("a.gradle", sortedUris.get(0));
+		assertUri("B.gradle", sortedUris.get(1));
+	}
+	
 	private void assertUri(String expected, URI actual) {
 		Assert.assertEquals(expected, FilenameUtils.getName(actual.toString()));
 		
