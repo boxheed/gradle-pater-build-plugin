1c5f8870305457ecce3d1cdb5c6df80b6176c942
boxheed <boxheed@users.noreply.github.com>
2015-02-04 13:53:19 +0000

    Added test for the classpath build file resolver.

---
+++ src/test/java/com/fizzpod/gradle/plugins/pater/ClasspathGradleBuildFileResolverTest.java
@@ -0,0 +1,17 @@
+package com.fizzpod.gradle.plugins.pater;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClasspathGradleBuildFileResolverTest {
+
+	@Test
+	public void testFindingBuildFile() {
+		ClasspathGradleBuildFileResolver resolver = new ClasspathGradleBuildFileResolver();
+		Collection<GradleBuildFile> buildFiles = resolver.findBuildFiles(null);
+		Assert.assertEquals(1, buildFiles.size());
+	}
+	
+}
