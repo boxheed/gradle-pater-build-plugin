f0b891dd187af3fbfae6634527cfe7f00659568a
Andrew Dunn <andy.dunn@expensys.com>
2015-02-08 22:28:47 +0000

    Updated the plugin to use Groovy iterators, and added hashcodes and equals.

--- src/main/groovy/com/fizzpod/gradle/plugins/pater/UriGradleBuildFile.groovy
+++ src/main/groovy/com/fizzpod/gradle/plugins/pater/UriGradleBuildFile.groovy
@@ -18,5 +18,36 @@ public class UriGradleBuildFile implements GradleBuildFile {
 	public void apply(Project project) {
 		project.apply(["from": uri]);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UriGradleBuildFile other = (UriGradleBuildFile) obj;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UriGradleBuildFile [uri=" + uri + ", getName()=" + getName()
+				+ "]";
+	}
 	
 }
